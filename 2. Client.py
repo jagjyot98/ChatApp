0fde68a1b856dc4e5d906e533f35946894534a50
Jagjyot Singh <52273126+jagjyot98@users.noreply.github.com>
2020-05-18 20:21:47 +0530

    Add files via upload

---
+++ 2. Client.py
@@ -0,0 +1,15 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = input(str("please enter hostname of server:"))  # taking system name to connect with
+s.connect((host, 1234))  # sending connection request to server for connection
+print("Connected to chat server")
+while 1:
+    msg = s.recv(1024)   # max amount of data to recieve in one message(bit size '1024')
+    msg = msg.decode()     # decpoding recieved messages from binary form to 'utf-8'
+    print("Server:", msg, end="\n\n")
+
+    message = input(str(">>"))
+    message = message.encode()   # encoding m.essage to be send from 'utf-8' to binary
+    s.send(message)    # sending message after encoding
+    print("Sent..", end="\n\n")
+    
